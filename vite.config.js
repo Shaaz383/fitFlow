ad5c94000da326ff2114bb78c1e0d3a06f2f2751
Syed Shaaz Akhtar <asadakhtar800@gmail.com>
2025-05-26 14:43:48 +0530

    Make changes for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,31 @@
-import path from 'path' // <-- Add this import at the top
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // Import the path module to resolve aliases
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    extensions: ['.js', '.jsx', '.json'] // Added extensions for better resolution
-
+    extensions: [".js", ".jsx", ".json"], // Added extensions for better resolution
   },
-})
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
